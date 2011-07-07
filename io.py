b4f5ae625e03ba1aa704e104f7be1ef42cf08a55
Praveen Kundurthy <pkundurthy@astro.washington.edu>
2011-07-07 11:09:45 -0700

    first commit

---
+++ io.py
@@ -0,0 +1,16 @@
+
+
+
+
+def ReadLightCurve(input arguments):
+    """ This function reads the Kepler database and returns ..."""
+
+
+def ReadKOITable(input parameters):
+    """ This function reads the Kepler database and returns ... """
+
+
+def checkifKnownPlanet(input parameters):
+    """ returns True is Kepler ID matches a planet in the datatable ... """
+    
+    
