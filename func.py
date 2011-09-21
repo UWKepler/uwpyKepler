ac132055e923e2c404f9231583501a978271e8df
pkundurthy <pkundurthy@astro.washington.edu>
2011-09-21 15:03:18 -0700

    comments added via github

--- func.py
+++ func.py
@@ -3,39 +3,50 @@ import numpy as num
 
 def getNumBool(List,bool):
     """ 
-    Returns the total number of certain booleans in a list
+    Returns the total number of booleans in a list
+    bool should be True or False
+    
+    e.g.
+    print len(getNumBool(List,True)), will list the number
+    of elements which are True in List.
     """
+    
     List = num.array(List)
     id0 = num.where(List == bool)[0]
     #print len(id0)
     return len(id0)
 
 def makeDTwindows(nsize,window):
-    """ Given a portion length and window size,
-        this function creates appropriate for detrending.
+    """ Given a portion length (nsize) and window size (window),
+        this function creates appropriate indicies
+        used to sclice windows for detrending.
     """
     
+    # two sets of slices for the cos**2 and sin**2 weights
     set1 = num.arange(0,nsize,window)
     set2 = num.arange(0+window/2,nsize,window)
     set2 = num.hstack( (0,set2) )
-    x = range(nsize)
+    
     outwin = {}
+    #diff, computes excess points near the edge of a portion
     diff1 = nsize - set1[-1]
     diff2 = nsize - set2[-1]
     if diff1 < window/2:
-        set1[-1] = set1[-1]+diff1
+        set1[-1] = set1[-1]+diff1	#adding excess points to index
     else:
-        set1 = num.hstack((set1,nsize))
+        set1 = num.hstack((set1,nsize)) #stacking slice index pairs
     if diff2 < window/2:
-        set2[-1] = set2[-1]+diff2
+        set2[-1] = set2[-1]+diff2	#adding excess points to index
     else:
-        set2 = num.hstack((set2,nsize))
+        set2 = num.hstack((set2,nsize)) #stacking slice index pairs
 
+    # for the special case when only 1 portion needs to be fit
     if len(set1) == 1:
         set1 = num.hstack((0,set1))
     if len(set2) == 1:
         set2 = num.hstack((0,set2))
 
+    # make pairs of the slicing indices for easier use
     outSet1 = makeRangePairs(set1)
     outSet2 = makeRangePairs(set2)
     idict = {1:outSet1,2:outSet2}
@@ -44,7 +55,8 @@ def makeDTwindows(nsize,window):
 
 def makeRangePairs(indexList):
     """
-    Given a list of indicies make a list of pairs.
+    Given a list of indicies make a list of pairs
+    using sequential indicies.
     """
     
     set = []
@@ -55,7 +67,9 @@ def makeRangePairs(indexList):
 
 def movingMedian(data,window):
     """
-    Returns the median smoothed version of a given function.
+    Returns the median smoothed version of given data.
+    data is the data (usually time series)
+    window is the size of the window used for smoothing
     """
     
     mvavg = []
@@ -69,7 +83,7 @@ def movingMedian(data,window):
     
 def compute1Sigma(data):
     """
-    return 1-sigma 
+    return 1-sigma
     """
     
     dsort = num.sort(data)
