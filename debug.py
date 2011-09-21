afc5235c9c12772c44a3efd65425cc8456ff0b5b
pkundurthy <pkundurthy@astro.washington.edu>
2011-09-21 14:54:15 -0700

    Edited to add comments via github (during computer downtime).

--- debug.py
+++ debug.py
@@ -2,6 +2,11 @@
 import sys, os
 
 def getUniqueTraceback(logFile):
+    """
+    sorts though exception log files and
+    prints unique exceptions to separate files
+    with a list of KIDs and input parameters.
+    """
     
     file = open(logFile,'r')
     fileLines = file.readlines()
@@ -13,7 +18,10 @@ def getUniqueTraceback(logFile):
     stkDict = {}
     Stack = ''
     StackList = []
+    #Read through each line in the log
     for i in range(len(fileLines)):
+        #Look for the start of a new error message
+        #and start writing the lines of this message
         if fileLines[i].startswith('Traceback'):
             errDict[erri] = Stack
             stkDict[erri] = Stack
@@ -21,19 +29,24 @@ def getUniqueTraceback(logFile):
             erri += 1
             StartStack = True
             Stack = ''
+        #Look for the start of the KID and 
+        #input parameter notes and write these lines
         if fileLines[i].startswith('#'):
             objDict[erri] = fileLines[i]
             StartStack = False
+        #Stack error lines
         if StartStack:
             Stack += fileLines[i]
 
-    print len(set(StackList[1:])), len(StackList[1:])
+    #print len(set(StackList[1:])), len(StackList[1:])
+    #Get the set of unique bugs and write to file
     UniqueBugs = list(set(StackList[1:]))
     UBugFile = open('UniqueBug.log','w')
     FOList = []
     for i in range(len(UniqueBugs)):
         print >> UBugFile, '# '+str(i+1)
         print >> UBugFile, UniqueBugs[i]
+        # Failed object (KID) list file
         bFileName = 'OBJ_BUG.'+str(i+1)+'.log'
         os.system('rm -v %s' % bFileName)
         FOList.append(open(bFileName,'a'))
