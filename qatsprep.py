3a248a49eefc98cfd5d01f6ac5de365d56197ae8
John Mehlhaff <johnm26@uw.edu>
2011-09-22 11:46:46 -0700

    Updated version of qatsprep.py

--- qatsprep.py
+++ qatsprep.py
@@ -5,48 +5,91 @@ def padLC(lcData):
     Pads missing cadences with ones.
     Returns the padded lightcurve.
     """
-                
-    newcadence = []
-    newtstamp = []
-    oldcadence = lcData['cadence']
-    efficiencynum = 1
-    iteration = 1
-    for i in range(len(oldcadence)):
-        if i < len(oldcadence)-1:
-            diffcadence = lcData['cadence'][i+1]-lcData['cadence'][i]
-            if diffcadence > 1:
-                diff = lcData['cadence'][i+1]-lcData['cadence'][i]
-                cnewidx = num.arange(diff-1)+i+1
-                tnewidx = num.arange(diff-1)+1
-                for j in cnewidx:
-                    if iteration == 0:
-                        newcadence.append(j)
-                        newtstamp.append(lcData['x'][i]+difftstamp)
-                        iteration =+ 1
-                    else: #Code in progress to not keep appending same timestamp when gap is larger than 1 point
-                        newcadence.append(j)
-                        newtstamp.append(lcData['x'][i+j]+difftstamp)
-            else:
-                while efficiencynum > 0:
-                    difftstamp = lcData['x'][i+1]-lcData['x'][i]
-                    efficiencynum -= 1
-
-    newcadence = num.array(newcadence, dtype='int64')
-    newtstamp = num.array(newtstamp, dtype='int64')
-    ones = num.zeros(len(newcadence))+1e0
-    #lcData['x'][newcadence] = 1
-    lcData['cadence'] = num.hstack((oldcadence,newcadence))
-    lcData['x'] = num.hstack((lcData['x'],newtstamp))
-    sortindex = lcData['cadence'].argsort()
-    #lcData['cadence'] = lcData['cadence'][sortindex]
-    #lcData['x'] = lcData['x'][sortindex]
-    for key in lcData.keys():
-        if key == ['ydt', 'yerrdt', 'y', 'yerr']:
-            lcData[key] = num.hstack((lcData[key],ones)) 
-            lcData[key] = lcData[key][sortindex]
-        if key == ['cadence', 'x']:
-            lcData[key] = lcData[key][sortindex]
+    newcadence = ['Placeholder']
+    newtstamp = ['Placeholder']
+    gaps = True
+    efficiencynum = 0
+    diffs = []
     
+    for i in range(len(lcData['cadence'])):
+        if i < len(lcData['cadence'])-1:
+            if lcData['cadence'][i+1]-lcData['cadence'][i] > 1:
+                diffs.append(i)
+                
+    gapnum = len(diffs)
+    
+    while gapnum > 0:
+        for i in range(len(lcData['cadence'])):
+            if i < len(lcData['cadence'])-1:
+                diffcadence = lcData['cadence'][i+1]-lcData['cadence'][i]
+                if diffcadence > 1:
+                    gapnum -= 1
+                    newcadence = []
+                    newtstamp = []
+                    diff = lcData['cadence'][i+1]-lcData['cadence'][i]
+                    cnewidx = num.arange(diff-1)+i+1 #Does not work after first gap: indices off. Problem addressed below
+                    tnewidx = num.arange(diff-1)
+                    for j in range(len(cnewidx)):
+                        if j == 0:
+                            newcadence.append(cnewidx[j])
+                            newtstamp.append(lcData['x'][i]+difftstamp)
+                        else: #Prevents appending same timestamp when gap is larger than 1 point
+                            newcadence.append(cnewidx[j])
+                            newtstamp.append(newtstamp[j-1]+difftstamp)
+                    ### Put inside for loop to reset lcData at each iteration, solving index problem above ###
+                    newcadence = num.array(newcadence, dtype='int64')
+                    newtstamp = num.array(newtstamp, dtype='float')
+                    ones = num.zeros(len(newcadence))+1e0
+                    lcData['cadence'] = num.hstack((lcData['cadence'],newcadence))
+                    lcData['x'] = num.hstack((lcData['x'],newtstamp))
+                    sortindex = lcData['cadence'].argsort()
+                    #for key in lcData.keys():
+                        #if key == 'ydt' or 'yerrdt' or 'y' or 'yerr':
+                            #print key
+                            #lcData[key] = num.hstack((lcData[key],ones)) 
+                            #lcData[key] = lcData[key][sortindex]
+                        #if key == 'cadence' or 'x':
+                            #lcData[key] = lcData[key][sortindex]
+                    lcData['ydt'] = num.hstack((lcData['ydt'],ones)) 
+                    lcData['yerrdt'] = num.hstack((lcData['yerrdt'],ones)) 
+                    lcData['y'] = num.hstack((lcData['y'],ones)) 
+                    lcData['yerr'] = num.hstack((lcData['yerr'],ones)) 
+                    lcData['ydt'] = lcData['ydt'][sortindex]
+                    lcData['yerrdt'] = lcData['yerrdt'][sortindex]
+                    lcData['y'] = lcData['y'][sortindex]
+                    lcData['yerr'] = lcData['yerr'][sortindex]
+                    lcData['cadence'] = lcData['cadence'][sortindex]
+                    lcData['x'] = lcData['x'][sortindex]
+                    break
+                else:
+                    if efficiencynum == 0:
+                        difftstamp = lcData['x'][i+1]-lcData['x'][i]
+                        efficiencynum += 1
+    
+    #delindex = []
+    #for i in lcData['x']:
+        #if len(num.where(lcData['x'][i] == lcData['x'][i])[0]) > 1:
+            #print len(num.where(lcData['x'][i] == lcData['x'][i])[0])
+            #delindex = num.where(lcData['x'][i] == lcData['x'][i])[0][1]
+            #lcData['cadence'] = lcData['cadence'].tolist()
+            #lcData['x'] = lcData['x'].tolist()
+            #lcData['ydt'] = lcData['ydt'].tolist()
+            #lcData['yerrdt'] = lcData['yerrdt'].tolist()
+            #lcData['y'] = lcData['y'].tolist()
+            #lcData['yerr'] = lcData['yerr'].tolist()
+            #del(lcData['cadence'][delindex])
+            #del(lcData['x'][delindex])
+            #del(lcData['ydt'][delindex])
+            #del(lcData['yerrdt'][delindex])
+            #del(lcData['y'][delindex])
+            #del(lcData['yerr'][delindex])
+            #lcData['cadence'] = num.array(lcData['cadence'])
+            #lcData['x'] = num.array(lcData['x'])
+            #lcData['ydt'] = num.array(lcData['ydt'])
+            #lcData['yerrdt'] = num.array(lcData['yerrdt'])
+            #lcData['y'] = num.array(lcData['y'])
+            #lcData['yerr'] = num.array(lcData['yerr'])
+            
     return lcData
     
     
