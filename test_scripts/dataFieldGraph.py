c2516d634ab647ac5ec1d00b2e66dd96fcdd9705
Christopher Martin <martincj@uw.edu>
2011-10-10 15:33:14 -0700

    Histogram plot in dataFIeldGraph.py

--- test_scripts/dataFieldGraph.py
+++ test_scripts/dataFieldGraph.py
@@ -2,9 +2,10 @@ import uwpyKepler as kep
 import pylab
 import numpy as num
 import sys
+import matplotlib.pyplot as plt
 
-skyNumber = sys.argv[1]
-
+#skyNumber = sys.argv[1]
+skyNumber = [1,2,3,4]
 #for i in range(skyNumber):
 	#skyGroup = kep.dbinfo.returnSkyGroupKIDs(skyNumber)
 
@@ -19,21 +20,55 @@ skyNumber = sys.argv[1]
 	#pylab.plot(FPCoords[0], FPCoords[1], 'ro')
 	#pylab.plot(PCCoords[0], PCCoords[1], 'co')
 	#pylab.legend(('SG='+str(skyNumber),'NFP='+str(len(FP)),'NPC='+str(len(PC))))
+lFP = []
+lPC = []
+lskyGroup = []
+print type(lFP)
+for i in skyNumber:
+    skyGroup = kep.dbinfo.returnSkyGroupKIDs(i)
+    
+    FP = kep.dbinfo.returnKIDsInKEPFP(skyGroup)
+    PC = kep.dbinfo.returnKIDsInKEPPC(skyGroup)
+    print type(lFP)
+    if len(FP) > 0:
+        lFP.append(len(FP))
+    else:
+        lFP.append(0)
+    if  len(PC) > 0:
+        lPC.append(len(PC))
+    else:
+        lPC.append(0)
+    
+    lskyGroup.append(len(skyGroup))
+    
+   
+    
 
-skyGroup = kep.dbinfo.returnSkyGroupKIDs(skyNumber)
+N=len(skyNumber)
+ind = num.arange(N)
+width = .35
 
-FP = kep.dbinfo.returnKIDsInKEPFP(skyGroup)
-PC = kep.dbinfo.returnKIDsInKEPPC(skyGroup)
+p1 = plt.bar(ind, lskyGroup, width, color='b')
+p2 = plt.bar(ind, lFP,  width, color='r', bottom=lFP)
+p3 = plt.bar(ind, lPC, width, color='y')
 
-FPCoords = kep.dbinfo.returnCoordKID(FP)
-PCCoords = kep.dbinfo.returnCoordKID(PC)
-skyCoords = kep.dbinfo.returnCoordKID(skyGroup)
+plt.ylabel('objects')
+plt.title('PC and FP vs all')
+#plt.xticks(ind+width/2., ('G1', 'G2', 'G3', 'G4', 'G5') )
+#plt.yticks(np.arange(0,81,10))
 
-pylab.plot(skyCoords[0], skyCoords[1], 'k.')
-pylab.plot(FPCoords[0], FPCoords[1], 'ro')
-pylab.plot(PCCoords[0], PCCoords[1], 'co')
-pylab.legend(('SG='+str(skyNumber),'NFP='+str(len(FP)),'NPC='+str(len(PC))))
-pylab.show()
+plt.show()
+
+    
+    #####FPCoords = kep.dbinfo.returnCoordKID(FP)
+    #####PCCoords = kep.dbinfo.returnCoordKID(PC)
+    #####skyCoords = kep.dbinfo.returnCoordKID(skyGroup)
+    
+    #####pylab.plot(skyCoords[0], skyCoords[1], 'k.')
+    #####pylab.plot(FPCoords[0], FPCoords[1], 'ro')
+    #####pylab.plot(PCCoords[0], PCCoords[1], 'co')
+    #####pylab.legend(('SG='+str(skyNumber),'NFP='+str(len(FP)),'NPC='+str(len(PC))))
+    #####pylab.show()
 
 #FPKids = kep.dbinfo.getKIDsFP()
 #PCKids = kep.dbinfo.getKIDsPC()
