2133abc02e968a8cfb601f87075e574259816a47
Christopher Martin <martincj@uw.edu>
2011-11-14 13:49:34 -0800

    Added detrend weights plot

--- test_scripts/testQATS_fromPipeline.py
+++ test_scripts/testQATS_fromPipeline.py
@@ -5,8 +5,8 @@ import pylab
 import numpy as num
 import cPickle as pickle
 
-par = kep.pipelinepars.PipelinePars('ps1')
-KIDList = kep.iofiles.readKIDsFromFile('KIDlist')
+par = kep.pipelinepars.PipelinePars('ParSet1')
+KIDList = kep.iofiles.readKIDsFromFile('KIDsExample.list')
 
 for trial in par.Trials(KIDList):
     tr = kep.pipelinepars.keptrial(trial)
@@ -14,13 +14,17 @@ for trial in par.Trials(KIDList):
     X = kep.keplc.keplc(tr.kid)
     X.runPipeline(kw)
     
-    Y = kep.qats.qatslc(X.lcFinal,X.KID)
-    Y.padLC()
-    Y.addNoise()
-    Y.runQATS()
-    #OutFile = open('ydata','wb')
-    #pickle.dump(Y,OutFile)
-    #OutFile.close()
-    pylab.plot(Y.periods,Y.SignalPower,'b-')
-    pylab.setp(pylab.gca().set_xscale('log'))
-    pylab.show()
+    #pylab.plot(X.lcFinal['x'], X.lcFinal['ydt'], 'b.')
+    #pylab.show()
+    
+    
+    #Y = kep.qats.qatslc(X.lcFinal,X.KID)
+    #Y.padLC()
+    #Y.addNoise()
+    #Y.runQATS()
+    ##OutFile = open('ydata','wb')
+    ##pickle.dump(Y,OutFile)
+    ##OutFile.close()
+    #pylab.plot(Y.periods,Y.SignalPower,'b-')
+    #pylab.setp(pylab.gca().set_xscale('log'))
+    #pylab.show()
