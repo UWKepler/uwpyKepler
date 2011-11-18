b657d98940bffbf327847273837c49e4c86e1129
John Mehlhaff <johnm26@uw.edu>
2011-11-18 15:34:24 -0800

    pwnplot.py can now fold with -f

--- test_plotting/pwnplot.py
+++ test_plotting/pwnplot.py
@@ -6,17 +6,21 @@ import numpy as num
 import pylab
 import optparse
 
-def rawplot(kid,ctype):
+def rawplot(kid,ctype,fold,bin):
     lcData = kep.iodb.ReadLightCurve(kid,selection=ctype)
-    #Look at difference between plot['x'] and bjdtime#
     BJDREFI = kep.iodb.getBJDREFI(kid)
+    x = lcData['x']+BJDREFI
+    if fold:
+        eData_idx = kep.keplc.keplc(kid).eData['KOI'].keys()[0]
+        eData = kep.keplc.keplc(kid).eData['KOI'][eData_idx]
+        x = kep.func.foldPhase(lcData,eData['T0'],eData['Period'])
     pylab.title('KID: %s' % kid)
     pylab.xlabel('BJD')
     pylab.ylabel('Flux')
-    pylab.plot(lcData['x']+BJDREFI, lcData['y'], 'b.')
+    pylab.plot(x, lcData['y'], 'b.')
     pylab.show()
 
-def pipelineplot(kid,ctype,pltlcFinal,dtopt,sharex):
+def pipelineplot(kid,ctype,pltlcFinal,dtopt,sharex,fold,bin):
     keplc = kep.keplc.keplc(kid)
     eData = keplc.eData
     BJDREFI = keplc.BJDREFI
@@ -31,18 +35,24 @@ def pipelineplot(kid,ctype,pltlcFinal,dtopt,sharex):
     agap=1,\
     durfac=2)
     
-    lcData = kep.keplc.lcData(kid,eData,BJDREFI, kw).lcData
+    lcData = kep.keplc.lcData(kid,eData,BJDREFI,kw).lcData
+    x = lcData['x']+BJDREFI
     
+    if fold:
+        eData_idx = kep.keplc.keplc(kid).eData['KOI'].keys()[0]
+        eData = kep.keplc.keplc(kid).eData['KOI'][eData_idx]
+        x = kep.func.foldPhase(lcData,eData['T0'],eData['Period'])
+
     if sharex:
         ugly = pylab.subplot(211)
         pylab.title('KID: %s' % kid)
         pylab.ylabel('Flux')
-        pylab.plot(lcData['x'], lcData['y'], 'b.')
-        pylab.plot(lcData['x'], lcData['correction'], 'k.')
+        pylab.plot(x, lcData['y'], 'b.')
+        pylab.plot(x, lcData['correction'], 'k.')
         pretty = pylab.subplot(212, sharex=ugly)
         pylab.xlabel('BJD')
         pylab.ylabel('CorrFlux')
-        pylab.plot(lcData['x'], lcData['ydt'], 'b.')
+        pylab.plot(x, lcData['ydt'], 'b.')
         pylab.show()
         exit()
     
@@ -52,24 +62,24 @@ def pipelineplot(kid,ctype,pltlcFinal,dtopt,sharex):
         pylab.title('KID: %s' % kid)
         pylab.xlabel('BJD')
         pylab.ylabel('CorrFlux')
-        pylab.plot(lcData['x']+BJDREFI, lcData['ydt'], 'b.')
+        pylab.plot(x, lcData['ydt'], 'b.')
         i+=1
     if dtopt:
         pylab.figure(i)
         pylab.title('KID: %s' % kid)
         pylab.xlabel('BJD')
         pylab.ylabel('Flux')
-        pylab.plot(lcData['x']+BJDREFI, lcData['y'], 'b.')
-        pylab.plot(lcData['x']+BJDREFI, lcData['correction'], 'k-')
+        pylab.plot(x, lcData['y'], 'b.')
+        pylab.plot(x, lcData['correction'], 'k.')
     pylab.show()
 
-def runpwn(kid,ctype,rawplt,pipelineplt,showDetrend,sharex,pipebool):
+def runpwn(kid,ctype,rawplt,pipelineplt,showDetrend,sharex,fold,bin,pipebool):
     if rawplt:
-        rawplot(kid,ctype)
+        rawplot(kid,ctype,fold,bin)
     if pipebool:
-        pipelineplot(kid,ctype,pipelineplt,showDetrend,sharex)
+        pipelineplot(kid,ctype,pipelineplt,showDetrend,sharex,fold,bin)
     if not rawplt and not pipebool:
-        rawplot(kid,ctype)
+        rawplot(kid,ctype,fold,bin)
 
 KID = sys.argv[1]
 if __name__ == '__main__':
@@ -99,6 +109,16 @@ if __name__ == '__main__':
                         default=False,\
                         help='plot the detrending function'\
                         ' and detrended data with a shared x-axis')
+    parser.add_option('-f','--fold',\
+                        action='store_true',\
+                        dest='fold',\
+                        default=False,\
+                        help='fold the lightcurve')
+    parser.add_option('-b','--bin',\
+                        action='store_true',\
+                        dest='bin',\
+                        default=False,\
+                        help='bin the lightcurve')
     cChoice = ('LC','SC','')
     parser.add_option('-c','--ctype',\
                         choices=cChoice,\
@@ -112,4 +132,4 @@ if __name__ == '__main__':
     pipebool = False
     if opts.pipelineplot or opts.showDetrend or opts.sharex:
         pipebool = True
-    runpwn(KID,opts.ctype,opts.rawplot,opts.pipelineplot,opts.showDetrend,opts.sharex,pipebool)
+    runpwn(KID,opts.ctype,opts.rawplot,opts.pipelineplot,opts.showDetrend,opts.sharex,opts.fold,opts.bin,pipebool)
