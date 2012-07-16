84999d4830be9c124298aabab6415d36355fe616
John Mehlhaff <johnm26@uw.edu>
2012-07-16 15:37:56 -0700

    transitFit.py equipped to mask known transit signals

--- test_plotting/transitFit.py
+++ test_plotting/transitFit.py
@@ -8,6 +8,7 @@ import scipy
 import sys
 import os
 import optparse
+import pdb
 
 # plots mod.ydata w/ or w/o errorbars
 # determined by errBool
@@ -110,6 +111,118 @@ def write_NoDuplicates(file, mod):
     for line in lines:
         print >> ofile, line.strip()
 
+# inserts fake eData
+# override inserts file eData even if eData is found in database
+def insertFakeEData(lc, **kwargs):
+    override = False
+    for kw in kwargs.keys():
+        if kw == 'override':
+            override = kwargs[kw]
+    if not lc.eData['eDataExists'] and not override:
+        if eDataInFile(kid):
+            periods, t0s, qs = alleDataFromFile(kid)
+            lc.eData['eDataExists'] = True
+            for i in range(len(periods)):
+                if tFlag and i == len(periods) - 1:
+                    rawlcData = \
+                        iodb.ReadLightCurve(kid, selection=ctype)
+                    rawlcData = \
+                        pipeline.FlagKeplerEvents(rawlcData)
+                    rawlcData = pipeline.RemoveBadEvents(rawlcData)
+                    eMask = \
+                        FlagEclipses(rawlcData,lc.eData,lc.BJDREFI)['eMask']
+                    excludeTransIdx = \
+                        num.where(FlagEclipses == False)[0]
+                lc.eData['KOI'] = {'fake' + str(i + 1): { \
+                    'Period':periods[i], \
+                    'T0':t0s[i], \
+                    'Duration':qs[i]}}
+            if tFlag:
+                return excludeTransIdx
+        else:
+            # crude estimates to be determined by fit
+            print 'no eData found; using hardcode guesses'
+            period = kep.postqats.getBestPeriodByKID(kid)
+            t0 = 60.
+            q = 1.5
+            lc.eData['KOI'] = {'fake': { \
+                'Period':period, \
+                'T0':t0, \
+                'Duration':q}}
+
+# returns ydata indices of all transits in catalogue and 
+# from periods, t0s, and qs from file except the last encountered
+def getKnownTransitIdx(lc, ctype):
+    rawlcData = kep.iodb.ReadLightCurve(lc.KID, selection=ctype)
+    rawlcData = kep.pipeline.FlagKeplerEvents(rawlcData)
+    rawlcData = kep.pipeline.RemoveBadEvents(rawlcData)
+    eMask = kep.pipeline.FlagEclipses( \
+        rawlcData,lc.eData,lc.BJDREFI)['eMask']
+    return num.where(eMask == False)[0]
+    
+# only used if no data found in file
+def insertCrudeEData(lc, fakeCount):
+    # crude estimates to be determined by fit
+    print 'no eData found in file; using hardcode guesses'
+    period = kep.postqats.getBestPeriodByKID(kid)
+    t0 = 60.
+    q = 1.5
+    lc.eData['KOI']['fake' + str(fakeCount)] = { \
+        'Period':period, \
+        'T0':t0, \
+        'Duration':q}
+    
+def multiMain(lc, nPlanets, ctype):
+    useCrude = False
+    if eDataInFile(lc.KID):
+        periods, t0s, qs = alleDataFromFile(lc.KID)
+    else:
+        useCrude = True
+    if lc.eData['eDataExists']:
+        if len(lc.eData['KOI']) > nPlanets or \
+        len(lc.eData['KOI']) + len(periods) > nPlanets + 1:
+            print 'recorded KOIs exceed expected planet count;'
+            print 'exiting'
+            exit()
+        #elif len(lc.eData['KOI']) == nPlanets:
+            #period, t0, q = geteDataFromFile(lc.KID)
+            #if period > 0:
+                #lc.eData['KOI']['fake1'] = { \
+                    #'Period':period, \
+                    #'T0':t0, \
+                    #'Duration':q}}
+            #else:
+                #insertCrudeEData(lc, 1)
+    if lc.eData['eDataExists']:
+        koiCount = len(lc.eData['KOI'])
+    else:
+        koiCount = 0
+    fakeCount = 1
+    while koiCount < nPlanets:
+        if not useCrude:
+            lc.eData['KOI']['fake' + str(fakeCount)] = { \
+                'Period':periods[fakeCount - 1], \
+                'T0':t0[fakeCount - 1], \
+                'Duration':q[fakeCount - 1]}
+            if fakeCount == len(periods):
+                useCrude = True
+        else:
+            insertCrudeEData(lc, fakeCount)
+        fakeCount += 1
+        koiCount  += 1
+    idx = getKnownTransitIdx(lc, ctype)
+    # insert remaining eData
+    if not useCrude:
+        for i in range(len(periods[fakeCount - 1:])):
+            lc.eData['KOI']['fake' + str(fakeCount + i)] = { \
+                'Period':periods[fakeCount - 1 + i], \
+                'T0':t0s[fakeCount - 1 + i], \
+                'Duration':qs[fakeCount - 1 + i]}
+    else:
+        insertCrudeEData(lc, fakeCount)
+    return idx
+
+
 if __name__ == '__main__':
     parser = optparse.OptionParser(usage=\
     "%prog\nFits lightcurve with Eric's transit model.\nScript takes a KID as a system argument.")
@@ -135,6 +248,13 @@ if __name__ == '__main__':
                         dest='error',\
                         default=False,\
                         help='plot ydata with errors')
+    parser.add_option('-m','--multiplanet',\
+                        type=float,\
+                        dest='nPlanets',\
+                        default=None,\
+                        help='input number known planets' \
+                        'in system;\nmasks these when calculating' \
+                        'fit')
     cChoice = ('LC','SC','')
     parser.add_option('-c','--ctype',\
                         choices=cChoice,\
@@ -148,32 +268,25 @@ if __name__ == '__main__':
 
 kid = sys.argv[1]
 lc = kep.keplc.keplc(kid)
-
-# insert fake eData
-if not lc.eData['eDataExists']:
-    if eDataInFile(kid):
-        period, t0, q = geteDataFromFile(kid)
-        lc.eData['eDataExists'] = True
-        lc.eData['KOI'] = {'fake':\
-                          {'Period':period, 'T0':t0, 'Duration':q}}
-    else:
-        # crude estimates to be determined by fit
-        period = kep.postqats.getBestPeriodByKID(kid)
-        t0 = 60.
-        q = 1.5
-        lc.eData['KOI'] = {'fake':\
-                          {'Period':period, 'T0':t0, 'Duration':q}}
+if opts.nPlanets:
+    Tidx = multiMain(lc, opts.nPlanets, opts.ctype)
 
 kw = kep.quicklc.quickKW(ctype=opts.ctype)
 lc.runPipeline(kw)
 lcData = lc.lcFinal
 eData = lc.eData
+
+if opts.nPlanets:
+    lcData['x'] = lcData['x'][Tidx]
+    lcData['ydt'] = lcData['ydt'][Tidx]
+    lcData['yerrdt'] = lcData['yerrdt'][Tidx]
+
 # estimate RpRs without picking up outliers
 ydt = lc.lcFinal['ydt']
 idx = ydt.argsort()
 iMin = idx[num.floor(0.1585*len(idx))]
 RpRsEst = num.sqrt(1. - ydt[iMin])
-# second element, aRs is FINNICKY;
+# aRs is FINNICKY;
 # fit can change drastically based guess!!
 # estimate aRs taking a = 1 / sin(q*pi) and q ~ 0.02
 aRsEst = 1. / num.sin(num.pi * 0.02)
