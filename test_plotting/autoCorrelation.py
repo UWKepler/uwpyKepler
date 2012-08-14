d30029a83172f95b8465ca59baf2f926199a9dbf
Christopher Martin <martincj@uw.edu>
2012-08-14 10:04:57 -0700

    added nhatQats, struct, and QatsHD to depracated

--- test_plotting/autoCorrelation.py
+++ test_plotting/autoCorrelation.py
@@ -9,15 +9,23 @@ import sys
 kid = sys.argv[1]
 
 #getting data
-keplc = kep.keplc.keplc(kid)
-eData = keplc.eData
-BJDREFI = keplc.BJDREFI
-kw = kep.quicklc.quickKW(ctype='LC')
+#keplc = kep.keplc.keplc(kid)
+#eData = keplc.eData
+#BJDREFI = keplc.BJDREFI
+#kw = kep.quicklc.quickKW(ctype='LC')
+lc = kep.keplc.keplc(kid)
+kw = kep.quicklc.quickKW()
+lc.runPipeline(kw)
+qlc = kep.qats.qatslc(lc.lcFinal, kid)
+qlc.padLC()
+
+x = qlc.lcData['x']
+y = qlc.lcData['y']
 
 #get lightcurve data after the pipeline has run on it
-lcData = kep.keplc.lcData(kid,eData,BJDREFI,kw).lcData
-x = lcData['x']+BJDREFI-2454900e0
-y = lcData['ydt']
+#lcData = kep.keplc.lcData(kid,eData,BJDREFI,kw).lcData
+#x = lcData['x']+BJDREFI-2454900e0
+#y = lcData['ydt']
 
 #running autocorrelation function
 crf = kep.func.autoCor(y)
@@ -28,13 +36,15 @@ daysIndex = (29.425*num.array(index))/(60.0*24.0)
 chopped = crf[5:-5]
 choppedIdx = index[5:-5]
 daysChoppedIdx = (29.425*num.array(choppedIdx))/(60.0*24.0)
+realx = num.array(x) - x[0]
+xChopped = realx[5:-5]
 
 #plotting results
 py.title('Correllation function of KID: %s' % kid)
 py.xlabel('Frequency')
 py.ylabel('Correlation function')
-py.plot(daysIndex, crf, '-r')
-py.plot(daysChoppedIdx, chopped, '-b')
+py.plot(realx, crf, '-r')
+py.plot(xChopped, chopped, '-b')
 py.grid(True)
 py.show()
 
