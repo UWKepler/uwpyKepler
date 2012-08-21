015c37d7a9a8f8a3c6c3516ae69fc65b79b5bc0f
Andy Becker <acbecker@gmail.com>
2012-08-21 12:03:37 -0700

    Confusion matrix debugging.

--- machineLearn.py
+++ machineLearn.py
@@ -4,18 +4,28 @@ import sklearn
 from sklearn.ensemble import RandomForestClassifier
 
 
-def plotConfusionMatrix(truth, predicted, labels):
+
+
+def plotConfusionMatrix(truth, predicted, labels, names):
     matrix = sklearn.metrics.confusion_matrix(truth, predicted, labels)
     fig = pylab.figure()
     ax  = fig.add_subplot(111)
-    ax.imshow(matrix)
+    im  = ax.imshow(matrix, interpolation='nearest', cmap = pylab.cm.binary)
+    for y in range(matrix.shape[0]):
+        for x in range(matrix.shape[1]):
+            ax.text(x, y, "%d" % (matrix[y][x]))
+
+    pylab.xticks(labels, names)
+    pylab.yticks(labels, names)
+    pylab.colorbar(im)
     pylab.show()
 
 def testConfusionMatrix():
     truth      = num.random.randint(0, 3, 500)
     predicted  = num.random.randint(0, 3, 500)
     labels     = [0, 1, 2]
-    plotConfusionMatrix(truth, predicted, labels)
+    names      = ["A", "B", "C"]
+    plotConfusionMatrix(truth, predicted, labels, names)
 
 if __name__ == '__main__':
     testConfusionMatrix()
